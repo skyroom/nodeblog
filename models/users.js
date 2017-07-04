b671c324f581a1c58d879176a6b929d68d31a96e
skygznb <wanghao@gznb.com>
2017-07-04 18:06:42 +0800

    add signup

--- models/users.js
+++ models/users.js
@@ -1,8 +1,9 @@
 var User = require('../lib/mongo').User;
-
+console.log('User', User);
 module.exports = {
     // 注册一个用户
     create: function create(user) {
         return User.create(user).exec();
+        // return User.insert(user).exec();
     }
 };
--- routes/signup.js
+++ routes/signup.js
@@ -44,6 +44,7 @@ router.post('/', checkNotLogin, function(req, res, next) {
     } catch (e) {
         // 注册失败，异步删除上传的头像
         fs.unlink(req.files.avatar.path);
+
         req.flash('error', e.message);
         return res.redirect('/signup');
     }
@@ -59,22 +60,27 @@ router.post('/', checkNotLogin, function(req, res, next) {
         bio: bio,
         avatar: avatar
     };
+
     // 用户信息写入数据库
     UserModel.create(user)
         .then(function(result) {
             // 此 user 是插入 mongodb 后的值，包含 _id
             user = result.ops[0];
+
             // 将用户信息存入 session
             delete user.password;
             req.session.user = user;
+
             // 写入 flash
             req.flash('success', '注册成功');
+
             // 跳转到首页
             res.redirect('/posts');
         })
         .catch(function(e) {
             // 注册失败，异步删除上传的头像
             fs.unlink(req.files.avatar.path);
+
             // 用户名被占用则跳回注册页，而不是错误页
             if (e.message.match('E11000 duplicate key')) {
                 req.flash('error', '用户名已被占用');
