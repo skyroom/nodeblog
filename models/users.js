803abb97afb8aedd2c3674706c523e80f8b47cc2
skycity <283362006@qq.com>
2017-07-04 22:28:25 +0800

    signout

--- models/users.js
+++ models/users.js
@@ -1,9 +1,10 @@
 var User = require('../lib/mongo').User;
-console.log('User', User);
+
 module.exports = {
     // 注册一个用户
     create: function create(user) {
         return User.create(user).exec();
         // return User.insert(user).exec();
+        // return User.create(user);
     }
 };
